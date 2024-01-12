28eed8f026a32e92fc8f6d5b246a567dd9b4b924
민감자 <mouse0429@naver.com>
2024-01-12 21:45:52 +0900

    feat(api): API 클래스 및 토큰 관리 기능

---
+++ lib/services/api.dart
@@ -0,0 +1,86 @@
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum HttpMethod {
+  get,
+  post,
+  put,
+  patch,
+  delete,
+}
+
+enum UrlType { dummy, dev }
+
+class API {
+  final String dummyUrl = dotenv.env['DUMMY_URL']!;
+  final String devUrl = dotenv.env['DEV_URL']!;
+
+  Future<http.Response> req(String url, HttpMethod method,
+      {body, UrlType type = UrlType.dummy, bool needToken = false}) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    if (needToken && prefs.getString("accessTokenExp") != null) {
+      final DateTime accessTokenExp =
+          DateTime.parse(prefs.getString("accessTokenExp")!);
+      if (accessTokenExp.isBefore(DateTime.now())) {
+        await refreshToken();
+      }
+    }
+
+    final reqUrl = Uri.parse(switch (type) {
+      UrlType.dev => devUrl + url,
+      UrlType.dummy => dummyUrl + url,
+    });
+
+    final header = {
+      if (method != HttpMethod.get) 'Content-Type': 'application/json',
+      if (needToken) 'Authorization': 'Bearer ${prefs.getString("accessToken")}'
+    };
+
+    switch (method) {
+      case HttpMethod.get:
+        return await http.get(reqUrl, headers: header);
+      case HttpMethod.post:
+        return await http.post(reqUrl, headers: header, body: body);
+      case HttpMethod.put:
+        return await http.put(reqUrl, headers: header, body: body);
+      case HttpMethod.patch:
+        return await http.patch(reqUrl, headers: header, body: body);
+      case HttpMethod.delete:
+        return await http.delete(reqUrl, headers: header, body: body);
+      default:
+        throw Exception("Invalid http method");
+    }
+  }
+
+  Future<void> refreshToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final res = await req(
+      '/auth/reissue',
+      HttpMethod.post,
+      body: jsonEncode(
+        {
+          'accessToken': prefs.getString("accessToken"),
+          'refreshToken': prefs.getString("refreshToken")
+        },
+      ),
+      type: UrlType.dev,
+    );
+    if (res.statusCode == 201) {
+      await saveToken(res.body);
+    } else {
+      throw Exception("http Exception");
+    }
+  }
+
+  Future<void> saveToken(res) async {
+    final body = jsonDecode(res.body)["data"];
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("accessToken", body["accessToken"]);
+    prefs.setString("refreshToken", body["refreshToken"]);
+    prefs.setString("accessTokenExp", body["accessTokenExp"]);
+    prefs.setString("refreshTokenExp", body["refreshTokenExp"]);
+  }
+}
