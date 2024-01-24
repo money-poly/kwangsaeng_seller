08bafd7b83788797b8080b7ab280f1a6cb8ce224
민감자 <mouse0429@naver.com>
2024-01-25 03:19:33 +0900

    feat(nav_view_model): 네비게이션 뷰 모델

---
+++ lib/screens/navigation/nav_view_model.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+enum NavItems {
+  home("홈"),
+  menu("메뉴 관리"),
+  setting("설정");
+
+  const NavItems(this.label);
+  final String label;
+}
+
+class NavViewModel with ChangeNotifier {
+  int _currIdx = 0;
+  final List<Widget> _pages = [
+    const Placeholder(),
+    const Placeholder(),
+    const Placeholder(),
+  ];
+
+  int get currIdx => _currIdx;
+  Widget get currPages => _pages[currIdx];
+
+  bool canChangeIdx(int idx) {
+    switch (idx) {
+      default:
+        return true;
+    }
+  }
+
+  void changeIdx(int idx) {
+    if (canChangeIdx(idx)) {
+      _currIdx = idx;
+    }
+    notifyListeners();
+  }
+}
