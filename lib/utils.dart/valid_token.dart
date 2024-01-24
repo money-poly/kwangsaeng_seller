d00af0fb0f201a433b75198995892e966ec94ea7
민감자 <mouse0429@naver.com>
2024-01-25 03:32:12 +0900

    feat(valid_token): 토큰 유효성 검증

---
+++ lib/utils.dart/valid_token.dart
@@ -0,0 +1,40 @@
+import 'package:kwangsaeng_seller/services/api.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<bool> checkValidToken() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final api = API();
+
+  final accessToken = prefs.getString("accessToken");
+  final refreshToken = prefs.getString("refreshToken");
+  final accessTokenExp = prefs.getString("accessTokenExp") != null
+      ? DateTime.parse(prefs.getString("accessTokenExp")!)
+      : null;
+  final refreshTokenExp = prefs.getString("refreshTokenExp") != null
+      ? DateTime.parse(prefs.getString("refreshTokenExp")!)
+      : null;
+  final storeId = prefs.getString("storeId");
+
+  print(
+      "[LOG] accessToken: $accessToken / refreshToken: $refreshToken / accessTokenExp: $accessTokenExp / refreshTokenExp: $refreshTokenExp / storeId: $storeId");
+
+  if (accessToken == null ||
+      refreshToken == null ||
+      accessTokenExp == null ||
+      refreshTokenExp == null ||
+      storeId == null) {
+    return false;
+  } else {
+    if (accessTokenExp.isBefore(DateTime.now()) ||
+        refreshTokenExp.isBefore(DateTime.now())) {
+      try {
+        api.refreshToken();
+        return true;
+      } catch (e) {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  }
+}
