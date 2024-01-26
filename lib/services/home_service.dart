860df1e93db1638833d35ca78e6650b535eabf21
민감자 <mouse0429@naver.com>
2024-01-26 15:59:08 +0900

    feat(home_service): 홈 서비스

---
+++ lib/services/home_service.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+import 'package:kwangsaeng_seller/models/store.dart';
+import 'package:kwangsaeng_seller/models/tag.dart';
+import 'package:kwangsaeng_seller/screens/home/home_view_model.dart';
+import 'package:kwangsaeng_seller/services/api.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HomeService {
+  final _api = API();
+
+  Future<HomeStore> getStoreInfo() async {
+    final prefs = await SharedPreferences.getInstance();
+    final res = await _api.req(
+        "/stores/basic/${prefs.getString("storeId")}", HttpMethod.get,
+        type: UrlType.dev, needToken: true);
+
+    if (res.statusCode == 200) {
+      return HomeStore.fromJson(jsonDecode(res.body)["data"]);
+    } else {
+      throw Exception("http Exception");
+    }
+  }
+
+  Future<List<Tag>> getTags() async {
+    final res = await _api.req("/tags", HttpMethod.get,
+        type: UrlType.dev, needToken: true);
+
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body)["data"]
+          .map<Tag>((e) => Tag.fromJson(e))
+          .toList();
+    } else {
+      throw Exception("http Exception");
+    }
+  }
+
+  Future<StoreStatus> updateStoreStatus(StoreStatus status) async {
+    final prefs = await SharedPreferences.getInstance();
+    final res = await _api.req(
+        "/stores/status/${prefs.getString("storeId")}", HttpMethod.patch,
+        type: UrlType.dev, needToken: true);
+
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body)["data"]["status"] == "open"
+          ? StoreStatus.open
+          : StoreStatus.closed;
+    } else {
+      throw Exception("http Exception");
+    }
+  }
+
+  Future<bool> updateStoreTag(int tagId) async {
+    final prefs = await SharedPreferences.getInstance();
+    final res = await _api.req(
+        "/stores/${prefs.getString("storeId")}", HttpMethod.put,
+        type: UrlType.dev, needToken: true, body: jsonEncode({"tagId": tagId}));
+
+    if (res.statusCode == 200) {
+      return true;
+    } else {
+      throw Exception("http Exception");
+    }
+  }
+}
