186808185eea0b9da607912da077ed55d3f18fe2
민감자 <mouse0429@naver.com>
2024-01-26 15:55:29 +0900

    fix(tag): tag 모델 수정

--- lib/models/tag.dart
+++ lib/models/tag.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
 class Tag {
+  int id;
   String icon;
   String name;
+  String description;
   Color txtColor;
   Color bgColor;
 
   Tag(
-      {required this.name,
+      {required this.id,
+      required this.icon,
+      required this.name,
+      required this.description,
       required this.txtColor,
-      required this.bgColor,
-      this.icon = "time"});
+      required this.bgColor});
+
+  factory Tag.fromJson(Map<String, dynamic> json) => Tag(
+        id: json['id'],
+        icon: json['name'],
+        name: json['content'],
+        description: json['description'],
+        txtColor: Color(int.parse(json['textColor'], radix: 16)),
+        bgColor: Color(int.parse(json['backgroundColor'], radix: 16)),
+      );
 }
