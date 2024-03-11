c09e94ee9b3cb6b7a266c392c308dcde2cbfd425
민감자 <mouse0429@naver.com>
2024-03-11 18:51:00 +0900

    feat(menu_register_view_model): 메뉴 등록 기능

--- lib/screens/menu/menu_register_view.dart
+++ lib/screens/menu/menu_register_view.dart
@@ -1,108 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kwangsaeng_seller/screens/menu/menu_register_view_model.dart';
+import 'package:kwangsaeng_seller/screens/menu/widgets/menu_origin_textfields.dart';
+import 'package:kwangsaeng_seller/screens/menu/widgets/menu_price_textfields.dart';
 import 'package:kwangsaeng_seller/styles/color.dart';
 import 'package:kwangsaeng_seller/styles/txt.dart';
 import 'package:kwangsaeng_seller/widgets/custom_btn.dart';
 import 'package:kwangsaeng_seller/widgets/custom_textfield.dart';
 import 'package:kwangsaeng_seller/widgets/img_upload_card.dart';
+import 'package:kwangsaeng_seller/widgets/loading_page.dart';
 import 'package:kwangsaeng_seller/widgets/textfield_title.dart';
+import 'package:provider/provider.dart';
 
 class MenuRegisterView extends StatelessWidget {
   const MenuRegisterView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = Provider.of<MenuRegisterViewModel>(context);
     return Stack(
       children: [
         KeyboardVisibilityBuilder(
           builder: (context, isKeyboardVisible) {
-            return Scaffold(
-              backgroundColor: KwangColor.grey100,
-              appBar: AppBar(
-                title: Text("메뉴 등록하기", style: KwangStyle.header2),
-                leading: GestureDetector(
-                  behavior: HitTestBehavior.translucent,
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: SvgPicture.asset("assets/icons/ic_36_back.svg",
-                        width: 36, height: 36),
-                  ),
-                ),
-                leadingWidth: 52,
-                titleSpacing: 0,
-                toolbarHeight: 52,
-                centerTitle: false,
+            return Form(
+              key: viewModel.formKey,
+              child: Scaffold(
                 backgroundColor: KwangColor.grey100,
-                elevation: 0,
-              ),
-              body: SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(20, 0, 20,
-                      144 + MediaQuery.of(context).viewPadding.bottom),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 20, top: 12),
-                        child: Stack(
-                          children: [
-                            const ImgUploadCard(imgUrl: null),
-                            Positioned(
-                              top: 0,
-                              right: 0,
-                              child: Container(
-                                width: 24,
-                                height: 24,
-                                padding: const EdgeInsets.all(3),
-                                decoration: BoxDecoration(
-                                  color: KwangColor.grey100,
-                                  shape: BoxShape.circle,
-                                  border: Border.all(color: KwangColor.grey500),
-                                ),
-                                child: SvgPicture.asset(
-                                    "assets/icons/ic_16_close.svg"),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      const TextFieldTitle(title: "메뉴명"),
-                      CustomTextField(
-                        controller: TextEditingController(),
-                        hintText: "메뉴명을 입력해주세요",
-                        validator: (input) {
-                          return null;
-                        },
-                        maxLength: 20,
-                        isVisibleMaxLength: true,
-                      ),
-                      const SizedBox(height: 20),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const TextFieldTitle(title: "정가"),
-                                CustomTextField(
-                                  controller: TextEditingController(),
-                                  hintText: "0",
-                                  inputFormatters: [
-                                    FilteringTextInputFormatter.digitsOnly
-                                  ],
-                                  keyboardType: TextInputType.number,
-                                  validator: (input) {
-                                    return null;
-                                  },
-                                  icon: Padding(
-                                    padding: const EdgeInsets.only(right: 16),
+                appBar: AppBar(
+                  title: Text("메뉴 등록하기", style: KwangStyle.header2),
+                  leading: GestureDetector(
+                    behavior: HitTestBehavior.translucent,
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: SvgPicture.asset("assets/icons/ic_36_back.svg",
+                          width: 36, height: 36),
+                    ),
+                  ),
+                  leadingWidth: 52,
+                  titleSpacing: 0,
+                  toolbarHeight: 52,
+                  centerTitle: false,
+                  backgroundColor: KwangColor.grey100,
+                  elevation: 0,
+                ),
+                body: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(20, 0, 20,
+                        144 + MediaQuery.of(context).viewPadding.bottom),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 20, top: 12),
+                          child: Stack(
+                            children: [
+                              GestureDetector(
+                                  onTap: () {},
                                   child:
                                       ImgUploadCard(imgUrl: viewModel.imgUrl)),
                               if (viewModel.imgUrl != null)
@@ -131,143 +90,66 @@ class MenuRegisterView extends StatelessWidget {
                           controller: viewModel.nameController,
                           hintText: "메뉴명을 입력해주세요",
                           validator: (input) {
+                            if (input.isEmpty) {
+                              return "메뉴명을 입력해주세요";
+                            }
                             return null;
                           },
                           maxLength: 20,
                           isVisibleMaxLength: true,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 8),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            "현재 ",
-                            style: KwangStyle.body2
-                                .copyWith(color: KwangColor.grey700),
-                          ),
-                          Text(
-                            "25.4% ",
-                            style: KwangStyle.body2M
-                                .copyWith(color: KwangColor.red),
-                          ),
-                          Text(
-                            "할인",
-                            style: KwangStyle.body2
-                                .copyWith(color: KwangColor.grey700),
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                      const TextFieldTitle(title: "메뉴 소개"),
-                      CustomTextField(
-                        controller: TextEditingController(),
-                        hintText:
-                            "메뉴를 소개하는 문구를 작성해주세요 (선택)\n예) 산미 없이 고소하게 즐기는 아메리카노",
-                        validator: (input) {
-                          return null;
-                        },
-                        maxLines: 5,
-                        maxLength: 100,
-                        isVisibleMaxLength: true,
-                      ),
-                      const SizedBox(height: 20),
-                      const TextFieldTitle(title: "원산지 표기"),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 8),
-                        child: Row(
-                          children: [
-                            Flexible(
-                              flex: 1,
-                              child: CustomTextField(
-                                controller: TextEditingController(),
-                                hintText: "재료명",
-                                validator: (input) {
-                                  return null;
-                                },
-                              ),
-                            ),
-                            const SizedBox(width: 8),
-                            Flexible(
-                              flex: 2,
-                              child: CustomTextField(
-                                controller: TextEditingController(),
-                                hintText: "원산지",
-                                validator: (input) {
-                                  return null;
-                                },
-                              ),
-                            ),
-                            const SizedBox(width: 8),
-                            GestureDetector(
-                              onTap: () {},
-                              behavior: HitTestBehavior.translucent,
-                              child: SvgPicture.asset(
-                                "assets/icons/ic_24_trash.svg",
-                                width: 24,
-                                height: 24,
-                                colorFilter: const ColorFilter.mode(
-                                    KwangColor.grey800, BlendMode.srcIn),
-                              ),
-                            )
-                          ],
                         ),
-                      ),
-                      Container(
-                          width: MediaQuery.of(context).size.width - 40,
-                          height: 52,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: KwangColor.grey400),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(
-                                "추가하기",
-                                style: KwangStyle.body1
-                                    .copyWith(color: KwangColor.grey600),
-                              ),
-                              const SizedBox(width: 4),
-                              SvgPicture.asset(
-                                "assets/icons/ic_16_add.svg",
-                                width: 16,
-                                height: 16,
-                              )
-                            ],
-                          ))
-                    ],
+                        const SizedBox(height: 20),
+                        MenuPriceTextFields(viewModel: viewModel),
+                        const SizedBox(height: 20),
+                        const TextFieldTitle(title: "메뉴 소개"),
+                        CustomTextField(
+                          controller: viewModel.descriptionController,
+                          hintText:
+                              "메뉴를 소개하는 문구를 작성해주세요 (선택)\n예) 산미 없이 고소하게 즐기는 아메리카노",
+                          validator: (input) {
+                            return null;
+                          },
+                          maxLines: 5,
+                          maxLength: 100,
+                          isVisibleMaxLength: true,
+                        ),
+                        const SizedBox(height: 20),
+                        MenuOriginTextFields(viewModel: viewModel),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              bottomSheet: Container(
-                padding: EdgeInsets.fromLTRB(
-                    20,
-                    16,
-                    20,
-                    isKeyboardVisible
-                        ? 16
-                        : 16 + MediaQuery.of(context).viewPadding.bottom),
-                decoration: const BoxDecoration(
-                  color: KwangColor.grey100,
-                ),
-                child: CustomBtn(
-                  txt: "등록하기",
-                  txtColor: KwangColor.grey100,
-                  bgColor: KwangColor.secondary400,
-                  onTap: () {},
+                bottomSheet: Container(
+                  padding: EdgeInsets.fromLTRB(
+                      20,
+                      16,
+                      20,
+                      isKeyboardVisible
+                          ? 16
+                          : 16 + MediaQuery.of(context).viewPadding.bottom),
+                  decoration: const BoxDecoration(
+                    color: KwangColor.grey100,
+                  ),
+                  child: CustomBtn(
+                    txt: "등록하기",
+                    txtColor: KwangColor.grey100,
+                    bgColor: KwangColor.secondary400,
+                    onTap: () async {
+                      viewModel.formKey.currentState!.validate();
+                      viewModel.validateAll();
+                      if (viewModel.checkAreAllValid()) {
+                        await viewModel.register().then((value) {
+                          if (value) Navigator.pop(context);
+                        });
+                      }
+                    },
+                  ),
                 ),
               ),
             );
           },
-        )
+        ),
+        if (viewModel.isRegisterLoading) const LoadingPage()
       ],
     );
   }
---
+++ lib/screens/menu/menu_register_view_model.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_styled_toast/flutter_styled_toast.dart';
+import 'package:kwangsaeng_seller/models/origin.dart';
+import 'package:kwangsaeng_seller/services/menu_service.dart';
+import 'package:tuple/tuple.dart';
+
+class MenuRegisterViewModel with ChangeNotifier {
+  final MenuService _service = MenuService();
+  final formKey = GlobalKey<FormState>();
+  bool _isRegisterLoading = false;
+  bool get isRegisterLoading => _isRegisterLoading;
+
+  MenuRegisterViewModel() {
+    _regularPriceController.addListener(() {
+      setDiscountRate();
+      checkAreValidPrices();
+    });
+    _discountPriceController.addListener(() {
+      setDiscountRate();
+      checkAreValidPrices();
+    });
+
+    origins[0].item1.addListener(() {
+      checkAreValidOrigins();
+    });
+    origins[0].item2.addListener(() {
+      checkAreValidOrigins();
+    });
+  }
+
+  /* 텍스트 컨트롤러 */
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _regularPriceController = TextEditingController();
+  final TextEditingController _discountPriceController =
+      TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  TextEditingController get nameController => _nameController;
+  TextEditingController get regularPriceController => _regularPriceController;
+  TextEditingController get discountPriceController => _discountPriceController;
+  TextEditingController get descriptionController => _descriptionController;
+
+  /* 기타 입력 값 */
+  String? _imgUrl;
+  final List<Tuple2<TextEditingController, TextEditingController>> _origins = [
+    Tuple2(TextEditingController(), TextEditingController())
+  ];
+  double _discountRate = 0;
+
+  String? get imgUrl => _imgUrl;
+  List<Tuple2<TextEditingController, TextEditingController>> get origins =>
+      _origins;
+  double get discountRate => _discountRate;
+
+  /* validated 값 */
+  bool _areValidatedPrices = false;
+
+  bool get areValidatedPrices => _areValidatedPrices;
+
+  /* 유효값 검증 */
+  bool _areValidPrices = false;
+  final List<bool> _areValidOrigins = [true];
+
+  bool get areValidPrices => _areValidPrices;
+  List<bool> get areValidOrigins => _areValidOrigins;
+
+  void setDiscountRate() {
+    var regularPrice = _regularPriceController.text.isEmpty
+        ? 0
+        : int.parse(_regularPriceController.text);
+    var discountPrice = _discountPriceController.text.isEmpty
+        ? 0
+        : int.parse(_discountPriceController.text);
+    if (regularPrice != 0 &&
+        discountPrice != 0 &&
+        regularPrice > discountPrice) {
+      _discountRate = (1 - (discountPrice / regularPrice)) * 100;
+    } else {
+      _discountRate = 0;
+    }
+    notifyListeners();
+  }
+
+  void addOrigin() {
+    _origins.add(Tuple2(TextEditingController(), TextEditingController()));
+    _areValidOrigins.add(true);
+    origins[origins.length - 1].item1.addListener(() {
+      checkAreValidOrigins();
+    });
+    origins[origins.length - 1].item2.addListener(() {
+      checkAreValidOrigins();
+    });
+    notifyListeners();
+  }
+
+  void removeOrigin(int idx) {
+    _origins.removeAt(idx);
+    _areValidOrigins.removeAt(idx);
+    notifyListeners();
+  }
+
+  void pickImage() {}
+
+  /* 유효성 검사 */
+  bool checkAreAllValid() {
+    if (isValidName() && _areValidPrices && !_areValidOrigins.contains(false)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool isValidName() {
+    return _nameController.text.isNotEmpty;
+  }
+
+  void validateAll() {
+    formKey.currentState!.validate();
+    _areValidatedPrices = true;
+    checkAreValidPrices();
+    checkAreValidOrigins();
+    notifyListeners();
+  }
+
+  void checkAreValidPrices() {
+    if (!arePricesNotEmpty()) {
+      _areValidPrices = false;
+    } else if (!isRegularBiggerThanDiscount()) {
+      _areValidPrices = false;
+    } else {
+      _areValidPrices = true;
+    }
+    notifyListeners();
+  }
+
+  bool arePricesNotEmpty() {
+    return _regularPriceController.text.isNotEmpty &&
+        _discountPriceController.text.isNotEmpty;
+  }
+
+  bool isRegularBiggerThanDiscount() {
+    return int.parse(_regularPriceController.text) >
+        int.parse(_discountPriceController.text);
+  }
+
+  void checkAreValidOrigins() {
+    for (var i = 0; i < _origins.length; i++) {
+      if (((origins[i].item1.text.isEmpty && origins[i].item2.text.isEmpty) ||
+          origins[i].item1.text.isNotEmpty &&
+              origins[i].item2.text.isNotEmpty)) {
+        areValidOrigins[i] = true;
+      } else {
+        areValidOrigins[i] = false;
+      }
+    }
+    notifyListeners();
+  }
+
+  /* 메뉴 등록 */
+  Future<bool> register() async {
+    changeIsRegistering(true);
+    if (await _service.registerMenu(
+        _nameController.text,
+        _descriptionController.text,
+        int.parse(regularPriceController.text),
+        int.parse(discountPriceController.text),
+        _discountRate,
+        origins
+            .where((e) => e.item1.text.isNotEmpty && e.item2.text.isNotEmpty)
+            .map((e) => Origin(ingredient: e.item1.text, country: e.item2.text))
+            .toList())) {
+      showToast("메뉴를 등록했습니다.");
+      changeIsRegistering(false);
+      return true;
+    } else {
+      showToast("메뉴 등록에 실패했습니다.");
+      changeIsRegistering(false);
+      return false;
+    }
+  }
+
+  void changeIsRegistering(bool isRegistering) {
+    _isRegisterLoading = isRegistering;
+    notifyListeners();
+  }
+}
