c9de5fee78d6e8b7edb15b136163fe58cd8b0ff8
민감자 <mouse0429@naver.com>
2024-03-14 01:11:32 +0900

    feat(menu): MenuDetail 모델

--- lib/models/menu.dart
+++ lib/models/menu.dart
@@ -1,6 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:kwangsaeng_seller/models/origin.dart';
+import 'package:kwangsaeng_seller/models/store.dart';
 import 'package:kwangsaeng_seller/models/tag.dart';
 import 'package:kwangsaeng_seller/styles/color.dart';
 import 'package:kwangsaeng_seller/utils.dart/menu_status_util.dart';
@@ -77,6 +78,71 @@ class Menu extends Equatable {
         origins: json['origins'],
       );
 
+  factory Menu.fromDetailJson(Map<String, dynamic> json) => Menu(
+        id: json['menuId'],
+        name: json['name'],
+        discountRate: json['discountRate'],
+        discountPrice: json['price'],
+        imgUrl: json['menuPictureUrl'],
+        /* Optional */
+        regularPrice: json['price'],
+        description: json['description'],
+        store: json['storeName'],
+        view: json['viewCount'] ?? json['view'],
+        tags: json['tags'],
+        origins: json['origins'],
+      );
+
   @override
   List<Object?> get props => [id];
+}
+
+class MenuDetail {
+  String name;
+  int discountRate;
+  int discountPrice;
+  int regularPrice;
+  StoreMenu store;
+  List<Menu> anotherMenus;
+  int view;
+  List<String> cautions;
+  List<Origin> origins;
+  /* Optional */
+  String? menuPictureUrl;
+  String? description;
+
+  MenuDetail({
+    required this.name,
+    required this.discountRate,
+    required this.discountPrice,
+    required this.regularPrice,
+    required this.store,
+    required this.anotherMenus,
+    required this.view,
+    required this.origins,
+    required this.cautions,
+    this.menuPictureUrl,
+    this.description,
+  });
+
+  factory MenuDetail.fromJson(Map<dynamic, dynamic> json) => MenuDetail(
+        name: json['name'],
+        discountRate: json['discountRate'],
+        discountPrice: json['sellingPrice'],
+        regularPrice: json['price'],
+        store: StoreMenu.fromJson(json['store']),
+        anotherMenus: (json['anotherMenus'] as List)
+            .map((e) => Menu.fromDetailJson(e))
+            .toList(),
+        view: json['viewCount'],
+        cautions: json['caution'].cast<String>(),
+        origins: json['countryOfOrigin'] == null
+            ? []
+            : (json['countryOfOrigin'] as List)
+                .map((e) => Origin.fromJson(e))
+                .toList(),
+        /* Optional */
+        menuPictureUrl: json['menuPictureUrl'],
+        description: json['description'],
+      );
 }
