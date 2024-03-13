f91bf3df19adae9b5a97dd126986f51eb869dfd7
민감자 <mouse0429@naver.com>
2024-03-14 01:06:14 +0900

    fix(loading_page): 로딩 페이지 확장
    - 배경 색상을 설정할 수 있도록 확장

--- lib/widgets/loading_page.dart
+++ lib/widgets/loading_page.dart
@@ -2,17 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:kwangsaeng_seller/styles/color.dart';
 
 class LoadingPage extends StatelessWidget {
-  const LoadingPage({super.key, this.color = KwangColor.grey400});
+  const LoadingPage(
+      {super.key,
+      this.color = KwangColor.grey400,
+      this.backgroundColor = Colors.transparent,
+      this.hasTopPadding = false});
 
   final Color color;
+  final Color backgroundColor;
+  final bool hasTopPadding;
   @override
   Widget build(BuildContext context) {
     return AbsorbPointer(
       absorbing: true,
-      child: Center(
-        child: CircularProgressIndicator(
-          strokeWidth: 4,
-          color: color,
+      child: Container(
+        padding:
+            hasTopPadding ? const EdgeInsets.only(top: 64) : EdgeInsets.zero,
+        color: backgroundColor,
+        child: Center(
+          child: CircularProgressIndicator(
+            strokeWidth: 4,
+            color: color,
+          ),
         ),
       ),
     );
